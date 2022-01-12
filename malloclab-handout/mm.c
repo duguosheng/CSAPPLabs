6977559f304c2caf9e0b623a77df11194973a1f5
duguosheng <2459513599@qq.com>
2022-01-12 18:57:40 +0800

    start mallocLab

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -1,6 +1,6 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -43,7 +43,7 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/* 
+/*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
@@ -51,42 +51,41 @@ int mm_init(void)
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
-void* mm_malloc(size_t size)
+void *mm_malloc(size_t size)
 {
     int newsize = ALIGN(size + SIZE_T_SIZE);
-    void* p = mem_sbrk(newsize);
-    if (p == (void*)-1)
+    void *p = mem_sbrk(newsize);
+    if (p == (void *)-1)
         return NULL;
     else {
-        *(size_t*)p = size;
-        return (void*)((char*)p + SIZE_T_SIZE);
+        *(size_t *)p = size;
+        return (void *)((char *)p + SIZE_T_SIZE);
     }
 }
 
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void* ptr)
-{
-}
+void mm_free(void *ptr)
+{}
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
-void* mm_realloc(void* ptr, size_t size)
+void *mm_realloc(void *ptr, size_t size)
 {
-    void* oldptr = ptr;
-    void* newptr;
+    void *oldptr = ptr;
+    void *newptr;
     size_t copySize;
 
     newptr = mm_malloc(size);
     if (newptr == NULL)
         return NULL;
-    copySize = *(size_t*)((char*)oldptr - SIZE_T_SIZE);
+    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
     if (size < copySize)
         copySize = size;
     memcpy(newptr, oldptr, copySize);
